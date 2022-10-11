1c9c8b374bd6945d0baae808b184d3a85e4e32b4
Bunyasit Fang <wachaa1319@gmail.com>
2022-10-11 10:35:29 +0700

    config native base

--- App.tsx
+++ App.tsx
@@ -11,24 +11,60 @@ import {
   extendTheme,
   VStack,
   Box,
+  ColorMode,
 } from "native-base";
+import type { StorageManager } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Define the config
 const config = {
   useSystemColorMode: false,
   initialColorMode: "dark",
+  dependencies: {
+    // For Expo projects (Bare or managed workflow)
+    "linear-gradient": require("expo-linear-gradient").LinearGradient,
+    // For non expo projects
+    // 'linear-gradient': require('react-native-linear-gradient').default,
+  },
+};
+
+const newColorTheme = {
+  brand: {
+    900: "#8287af",
+    800: "#7c83db",
+    700: "#b3bef6",
+  },
 };
 
 // extend the theme
-export const theme = extendTheme({ config });
+export const theme = extendTheme({ colors: newColorTheme });
 type MyThemeType = typeof theme;
 declare module "native-base" {
   interface ICustomTheme extends MyThemeType {}
 }
+
 export default function App() {
+  const colorModeManager: StorageManager = {
+    get: async () => {
+      try {
+        let val = await AsyncStorage.getItem("@my-app-color-mode");
+        return val === "dark" ? "dark" : "light";
+      } catch (e) {
+        console.log(e);
+        return "light";
+      }
+    },
+    set: async (value: ColorMode) => {
+      try {
+        await AsyncStorage.setItem("@my-app-color-mode", value);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  };
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme} colorModeManager={colorModeManager}>
       <Center
         _dark={{ bg: "blueGray.900" }}
         _light={{ bg: "blueGray.50" }}
@@ -49,7 +85,7 @@ export default function App() {
               }}
               px={2}
               py={1}
-              _dark={{ bg: "blueGray.800" }}
+              _dark={{ bg: "brand.700" }}
               _light={{ bg: "blueGray.200" }}
             >
               App.js
@@ -57,7 +93,7 @@ export default function App() {
             <Text>and save to reload.</Text>
           </HStack>
           <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
+            <Text color="brand.800" underline fontSize={"xl"}>
               Learn NativeBase
             </Text>
           </Link>
