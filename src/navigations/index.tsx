f622872d2eb206dcb35ce5155edb09b440c519d4
Bunyasit Fang <wachaa1319@gmail.com>
2023-05-12 14:38:56 +0700

    tabbar icons

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -28,7 +28,7 @@ import { useUserContext } from "../hooks";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Icon, Text, useColorModeValue } from "native-base";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import Svg, { Path, SvgFromUri, SvgUri } from "react-native-svg";
+import Svg, { Path, SvgFromUri, SvgUri, SvgXml } from "react-native-svg";
 import { RemixIcons } from "../../assets/Icons/RemixIcons";
 import { BlurView } from "expo-blur";
 import { StyleSheet } from "react-native";
@@ -247,6 +247,18 @@ function EventStackNavigator() {
     </EventStack.Navigator>
   );
 }
+const icons = {
+  apps: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M6.75 2.5A4.25 4.25 0 0 1 11 6.75V11H6.75a4.25 4.25 0 1 1 0-8.5zm0 10.5H11v4.25A4.25 4.25 0 1 1 6.75 13zm10.5-10.5a4.25 4.25 0 1 1 0 8.5H13V6.75a4.25 4.25 0 0 1 4.25-4.25zM13 13h4.25A4.25 4.25 0 1 1 13 17.25V13z"/></svg>`,
+
+  "apps-outline": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M6.75 2.5A4.25 4.25 0 0 1 11 6.75V11H6.75a4.25 4.25 0 1 1 0-8.5zM9 9V6.75A2.25 2.25 0 1 0 6.75 9H9zm-2.25 4H11v4.25A4.25 4.25 0 1 1 6.75 13zm0 2A2.25 2.25 0 1 0 9 17.25V15H6.75zm10.5-12.5a4.25 4.25 0 1 1 0 8.5H13V6.75a4.25 4.25 0 0 1 4.25-4.25zm0 6.5A2.25 2.25 0 1 0 15 6.75V9h2.25zM13 13h4.25A4.25 4.25 0 1 1 13 17.25V13zm2 2v2.25A2.25 2.25 0 1 0 17.25 15H15z"/></svg>`,
+
+  home: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <g> <path fill="none" d="M0 0h24v24H0z"/> <path d="M20 20a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9z"/> </g> </svg>
+`,
+  "home-outline": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g> <path fill="none" d="M0 0h24v24H0z"/><path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19z"/> </g> </svg> `,
+  "compass-outline": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <g> <path fill="none" d="M0 0h24v24H0z"/> <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm3.5-11.5l-2 5-5 2 2-5 5-2z"/> </g> </svg> `,
+  compass: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <g> <path fill="none" d="M0 0h24v24H0z"/> <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm3.5-13.5l-5 2-2 5 5-2 2-5z"/> </g> </svg> `,
+};
+
 const TabBarIcon =
   (name: "apps" | "compass" | "home") =>
   ({
@@ -264,10 +276,16 @@ const TabBarIcon =
       //   size={30}
       //   name={focused ? name : `${name}-outline`}
       // />
-      <Icon
-        as={Ionicons}
-        color={color}
-        size={30}
-        name={focused ? name : `${name}-outline`}
+      // <Icon
+      //   as={Ionicons}
+      //   color={color}
+      //   size={30}
+      //   name={focused ? name : `${name}-outline`}
+      // />
+      <SvgXml
+        xml={icons[focused ? name : (`${name}-outline` as keyof typeof icons)]}
+        width={30}
+        height={30}
+        fill={color}
       />
     );
